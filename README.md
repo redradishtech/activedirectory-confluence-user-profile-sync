07d555d47fd32706531c3922e8c7338dfd45482f
Jeff Turner <jeff@redradishtech.com>
2013-09-05 00:30:24 +1000

    Initial version

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-activedirectory-confluence-user-profile-sync
-============================================
+# Active Directory to Confluence User Profile Synchronizer
 
-Active Directory to Confluence User Profile Synchronizer
+This is a command-line utility that copies user records (telephone number, jabber ID, department, location) from Active Directory to Atlassian Confluence users' profiles.
+
+## Sample Usage
+
+$ ./sync_ad_confluence.rb \
+    --ldaphost "example.ds" \
+    --binddn="CN=Service Account for LDAP search and Bind,OU=SERVICE ACCOUNTS,OU=ADMIN,DC=example,DC=ds" \
+    --bindpassword="s3cret" \
+    --basedn="OU=ADMIN,DC=example,dc=ds" \
+    --confbaseurl="http://wiki.example.ds" \
+    --confuser="admin" \
+    --confpassword="confs3cret" \
+    admin
+Updated http://wiki.example.ds/admin/users/viewuser.action;jsessionid=4501D470FE1ACD4A32638FC3BB4EFD32?username=admin
---
+++ sync_ad_confluence.rb
@@ -0,0 +1,176 @@
+#!/usr/bin/env ruby
+
+require 'trollop'
+require 'uri'
+require 'net/http'
+require 'net/ldap'
+require 'json'
+require 'open-uri'
+require 'nokogiri'
+require 'parallel'
+
+opts = Trollop::options do
+    banner <<-EOS
+Active Directory-to-Confluence user profile synchronizer
+
+Can be used to sync a specific user, or (by default) all AD users with a Confluence account.
+
+Usage: sync_ad_confluence [options] [username]
+EOS
+
+    opt :ldaphost, "LDAP hostname, eg. 'mycompany.ds'", :short => 'h', :type=>:string, :default => (ENV['LDAP_HOST'])
+    opt :ldapport, "LDAP port number", :short => 'p', :default => (ENV['LDAP_PORT'] || 389)
+    opt :binddn, "LDAP bind DN, eg. 'CN=Syncer,OU=ADMIN,DC=Company,DC=ds'", :short => 'D',:type=>:string, :default => (ENV['LDAP_BINDDN'])
+    opt :bindpassword, "LDAP bind password", :short => 'w', :type=>:string, :default => (ENV['LDAP_BINDPASSWORD'])
+    opt :basedn, "LDAP base DN, eg. 'OU=ADMIN,DC=Company,DC=ds'", :short => 'b', :type=>:string, :default => (ENV['LDAP_BASEDN'])
+    opt :confbaseurl, "Confluence base URL, eg. 'http://wiki.company.com'", :short => 'B', :type=>:string, :default => (ENV['CONF_BASEURL'])
+    opt :confuser, "Confluence username", :short=>'U', :type => :string, :default => (ENV['CONF_USER'])
+    opt :confpassword, "Confluence password", :short=>'P', :type => :string, :default => (ENV['CONF_PASSWORD'])
+end
+
+
+def get_edituser_actionurl(opts, username)
+
+    uri = URI(opts[:confbaseurl] + "/admin/users/edituser.action?os_authType=basic&username=#{username}")
+    req = Net::HTTP::Get.new(uri)
+    req.basic_auth opts[:confuser], opts[:confpassword]
+    req.add_field("X-Atlassian-Token", "no-check")
+    http = Net::HTTP.new(uri.host, uri.port)
+    #http.set_debug_output $stderr
+    res = http.request(req)
+    case res
+    when Net::HTTPSuccess
+	if res.body =~ /Not Permitted/ then
+	    $stderr.puts "Could not find #{username}"
+	    exit 1
+	end
+	#puts "Successfully located user #{username}"
+	html = Nokogiri::HTML(res.body)
+	actionurl = html.css("form[name=editUser]").attribute("action").value
+	return actionurl
+    else
+	raise "Lookup of user #{username} failed"
+	p res
+	res.value
+	exit 1
+    end
+end
+
+# Iterate through AD users
+def activedirectory_users(opts, accountname_expr = 'jturner')
+
+    ldap = Net::LDAP.new :host => opts[:ldaphost],
+	:port => opts[:ldapport],
+	:auth => {
+	:method => :simple,
+	:username => opts[:binddn],
+	:password => opts[:bindpassword]
+    
+    }
+
+    #filter = Net::LDAP::Filter.eq("objectCategory", "Person*")
+    filter = Net::LDAP::Filter.construct("(&(objectCategory=Person)(sAMAccountName=#{accountname_expr}))")
+
+    ldap.search(
+	:base => opts[:basedn],
+	:filter => filter,
+	:attributes => [:samaccountname, :displayname, :mail, :telephonenumber, :description, :department, :company, :physicaldeliveryofficename, :streetaddress, :l, :st, :postalcode, :co, :thumbnailPhoto]
+    ) 
+end
+
+def update_confluence_profile(opts, fields)
+    username = fields[:username]
+    fullname = fields[:fullname]
+    email = fields[:email]
+    raise 'username must be present' unless username
+    raise 'fullname must be present' unless fullname
+    raise 'email must be present' unless email
+
+    actionurl = get_edituser_actionurl(opts, username)
+    uri = URI("#{opts[:confbaseurl]}/admin/users/#{actionurl}")
+    http = Net::HTTP.new(uri.host, uri.port)
+    #http.set_debug_output $stderr
+    req = Net::HTTP::Post.new(uri)
+    req.basic_auth opts[:confuser], opts[:confpassword]
+    req.add_field("X-Atlassian-Token", "no-check")
+
+    params = {'os_authType' => 'basic',
+	'username' => username,
+	'fullName' => fullname,
+	'email' => email,
+	'confirm' => 'Submit'
+    }
+    fields.each { |k,v| 
+	params['userparam-' + k.to_s] = v
+    }
+    req.set_form_data(params)
+    res = http.request(req)
+    case res
+    when Net::HTTPRedirection
+	# We expect to be redirected to the user's profile, and this function returns that link
+	if res["Location"] =~ /authenticate\.action/ then
+	    $stderr.puts "Please disable Secure administrator sessions aka websudo"
+	    exit 1
+	end
+	return res["Location"]
+    #when Net::HTTPSuccess
+#	puts res.body
+    else
+	raise "Unexpected HTTP response #{res.value} setting fields #{fields}: #{res}"
+    end
+end
+
+# Given a hash of activedirectory attributes (eg. {displayname: ["Joe Bloggs"], mail: [joe@example.com], ...}, return a hash of equivalent JIRA user profile HTML form field values.
+# 
+# Sample keys that might be passed in
+#:attributes => [:samaccountname, :displayname, :mail, :telephonenumber, :description, :department, :company, :physicaldeliveryofficename, :streetaddress, :l, :st, :postalcode, :co]
+def ad_to_profile(adhash)
+    confhash={}
+    o,s,l,st,p,co=nil
+    adhash.each { |k,v|
+	case k
+	when :samaccountname
+	    confhash[:username]=v[0]
+	when :displayname
+	    confhash[:fullname]=v[0]
+	when :mail
+	    confhash[:email]=v[0]
+	when :telephonenumber
+	    confhash[:phone]=v[0]
+	when :jabberid
+	    confhash[:im]=v[0]
+	when :description
+	    confhash[:position]=v[0]
+	when :department
+	    confhash[:description]=v[0]
+	when :physicaldeliveryofficename
+	    o=v[0]
+	when :streetaddress
+	    s=v[0]
+	when :l
+	    l=v[0]
+	when :st
+	    st=v[0]
+	when :postalcode
+	    p=v[0]
+	when :co
+	    co=v[0]
+	end
+    }
+    # Location is a composite of various AD fields
+    confhash[:location]=[o,s,l,st,p,co].select{|x|x}.join(", ")
+    return confhash
+end
+
+
+
+username=ARGV.shift || '*'
+Parallel.map( activedirectory_users(opts, username), :in_processes=>10 ) { |entry|
+
+    profilefields = ad_to_profile(entry)
+    if !profilefields[:email] then
+	$stderr.puts " *** Skipping #{profilefields[:username]} as it has no email record"
+    else
+	puts "Updated " + update_confluence_profile(opts, profilefields)
+    end
+}
